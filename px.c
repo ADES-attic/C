0163c25378007ce0a1311fcb2433cb877e2cf66d
Sonia Keys <skeys@cfa.harvard.edu>
2015-08-26 22:29:13 +0000

    convert a header line

--- px.c
+++ px.c
@@ -1,16 +1,136 @@
 #include <libxml/tree.h>
+#include <string.h>
 
-int main()
+// globals
+FILE *fpsv;
+int lineNum;
+xmlNodePtr root_node;
+char line[512];
+char kw[31];
+
+// fatal
+//
+// print message to stdout and terminate program.
+void fatal(char *msg)
+{
+  fputs(msg, stderr);
+  fputc('\n', stderr);
+  exit(-1);
+}
+
+void fatal1(char *msg, char *arg)
+{
+  sprintf(line, msg, arg);
+  fatal(line);
+}
+
+void fatalPSV(char *msg)
+{
+  sprintf(line, "PSV line %d: %s", lineNum, msg);
+  fatal(line);
+}
+
+char *getPSVLine()
+{
+  // TODO fgets bad, getline better?
+  char *p = fgets(line, sizeof(line), fpsv);
+  if (p)
+    lineNum++;
+  return p;
+}
+
+int trimSpace(char *s, char **start)
+{
+  // TODO handle general whitespce
+  char *s2 = s + strspn(s, " ");
+  *start = s2;
+  size_t len = strlen(s2);
+  while (len > 0 && s2[len - 1] == ' ')
+    len--;
+  return len;
+}
+
+void pxObs()
+{
+}
+
+// pxHeader
+//
+// on entry `line` contains a header line.
+// parse the observation header into the tree, skip blank lines,
+// on non-header, non-blank line, call pxObs.
+void pxHeader()
+{
+  xmlNodePtr hdr = xmlNewChild(root_node, NULL,
+                               BAD_CAST "observationContext", NULL);
+
+  // handle a single # line
+  char *kwStart = line + 1 + strspn(line + 1, " ");
+  if (!*kwStart)
+    fatalPSV("missing header keyword");
+  char *kwEnd = strchr(kwStart, ' ');
+  size_t kwLen = kwEnd ? kwEnd - kwStart : strlen(kwStart);
+  if (kwLen > sizeof(kw) - 1)
+    fatalPSV("long keyword");
+  strncpy(kw, kwStart, kwLen);
+  if (!xmlCheckUTF8(kw))
+    fatalPSV("invalid characters present");
+  char *txt = NULL;
+  if (kwEnd) {
+    kwLen = trimSpace(kwEnd, &txt);
+    if (!kwLen)
+      txt = NULL;
+    else {
+      txt[kwLen] = 0;
+      if (!xmlCheckUTF8(txt))
+        fatalPSV("invalid characters present");
+    }
+  }
+  xmlNodePtr h1 = xmlNewChild(hdr, NULL, BAD_CAST kw, BAD_CAST txt);
+}
+
+// px
+//
+// top level loop, called after .psv is open and root_node is created.
+//
+// loop:
+//   skip blank lines
+//   if '#', start batch, parse header
+//   else start batch, parse obs
+void px()
+{
+  if (!getPSVLine())
+    fatal("can't read psv");
+  do {
+    if (line[strlen(line) - 1] == '\n')
+      line[strlen(line) - 1] = 0;
+    if (*line == '#')
+      pxHeader();
+    else if (*line != 0)
+      pxObs();
+  }
+  while (getPSVLine());
+}
+
+// main
+//
+// usage: px <input .psv file> <output .xml file>
+int main(int argc, char **argv)
 {
   LIBXML_TEST_VERSION;
+  if (argc != 3) {
+    fatal("usage: px <input .psv file> <output .xml file>");
+  }
+  fpsv = fopen(argv[1], "r");
+  if (!fpsv) {
+    fatal1("can't open %s", argv[1]);
+  }
 
   xmlDocPtr doc = xmlNewDoc(BAD_CAST "1.0");
-  xmlNodePtr root_node = xmlNewNode(NULL, BAD_CAST "ObservationBatch");
+  root_node = xmlNewNode(NULL, BAD_CAST "ObservationBatch");
   xmlDocSetRootElement(doc, root_node);
 
-  xmlNodePtr obs = xmlNewChild(root_node, NULL, BAD_CAST "observations", NULL);
-  xmlNodePtr opt = xmlNewChild(obs, NULL, BAD_CAST "optical", NULL);
-  xmlNewChild(opt, NULL, BAD_CAST "trkSub", BAD_CAST "P01abcd");
+  px();
 
-  xmlSaveFormatFile("-", doc, 1);
+  xmlSaveFormatFileEnc(argv[2], doc, "UTF-8", 1);
 }
