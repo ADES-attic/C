32cbc54b95c26359a382713c6ad866ebe251bd07
Sonia Keys <skeys@cfa.harvard.edu>
2015-08-31 19:40:07 +0000

    parse column headers, includes changes to whitespace functions

--- px.c
+++ px.c
@@ -1,18 +1,21 @@
-#include <libxml/tree.h>
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <string.h>
+
+#include <libxml/tree.h>
 
 // globals
 FILE *fpsv;
 int lineNum;
-xmlDocPtr doc;
-xmlNodePtr root_node;
 char line[512];
 char kw[31];
+xmlDocPtr doc;
+xmlNodePtr root_node;
 
 // fatal - print message to stdout and terminate program.
 //
-// fatal always adds a newline.  typically msg should not have a trailing
-// newine.
+// fatal always adds a newline.
+// typically msg should not have a trailing newine.
 void fatal(char *msg)
 {
   fputs(msg, stderr);
@@ -34,15 +37,17 @@ void fatalPSV(char *msg)
   fatal(m);
 }
 
-#define LATIN1_WS "\t\n\v\f\r \x85\xA0"
 // TODO figure out the rest of Unicode whitespace. (white space property?
 // category Z? related functions is libxml2? in libunistring?)
+#define LATIN1_WS "\t\n\v\f\r \x85\xA0"
 
 // trimRight trims white space from the right of a string, by punching a
 // null terminator byte.
 //
 // arg is an xmlChar*.  trimRight scans the entire string; any non UTF-8 is
 // detected and is a fatal error.
+//
+// return value is the number of whitespace bytes trimed.
 size_t trimRight(xmlChar * p)
 {
   xmlChar *wsStart = NULL;
@@ -69,6 +74,34 @@ size_t trimRight(xmlChar * p)
   return p - wsStart;
 }
 
+// firstNonWS returns a pointer to the first non-whitespace character in p.
+//
+// p must be a null terminated string.
+char *firstNonWS(char *p)
+{
+  int all = strlen(p);
+  int c, len;
+  while (*p) {
+    len = all;
+    c = xmlGetUTF8Char(p, &len);
+    if (!xmlStrchr(LATIN1_WS, c))
+      break;
+    p += len;
+    all -= len;
+  }
+  return p;
+}
+
+// trim trims both leading and trailing whitespace.
+//
+// a pointer to first non-whitespace character is returned.
+// a null is punched to the last whitespace character from the end.
+char *trim(char *s)
+{
+  trimRight(s);
+  return firstNonWS(s);
+}
+
 // get non-blank line, validate UTF-8, trim trailing space.
 char *getPSVLine()
 {
@@ -85,8 +118,45 @@ char *getPSVLine()
   return p;
 }
 
+// On entry `line` must contain the PSV column headers
+char **splitColHdrs(int *nCols)
+{
+  // count pipes first and allocate array for headers
+  int nPipes = 0;
+  for (const char *p = line; p = xmlStrchr(p, '|'); p++)
+    nPipes++;
+  *nCols = nPipes + 1;
+  char **colHdrs = malloc(*nCols * sizeof(char *));
+
+  // scan again, filling header array
+  char *cs = strdup(line);
+  colHdrs[0] = cs;
+  int i = 1;
+  // cast away const
+  for (char *p = cs; p = (char *)xmlStrchr(p, '|');) {
+    *p++ = 0;
+    colHdrs[i++] = p;
+  }
+
+  // trim headers, catch blank headers
+  for (int i = 0; i < *nCols; i++) {
+    char *p = trim(colHdrs[i]);
+    if (!*p)
+      fatalPSV("empty column header");
+    colHdrs[i] = p;
+  }
+
+  printf("%d headers:\n", *nCols);
+  for (int i = 0; i < *nCols; i++)
+    printf("  <%s>\n", colHdrs[i]);
+  return colHdrs;
+}
+
+// On entry `line` must contain the PSV column headers
 void pxObs()
 {
+  int nCols;
+  char **colHdrs = splitColHdrs(&nCols);
 }
 
 // handle a single header line, either a # or ! line.
@@ -94,25 +164,17 @@ xmlNodePtr addHdr(xmlNodePtr parent)
 {
   // find keyword start.  first byte of line is known to be # or !,
   // white space can follow, then a keyword must start.
-  char *kwStart = line + 1;
-  int all = strlen(kwStart);
-  int c, len;
-  while (1) {
-    if (!*kwStart)
-      fatalPSV("missing header keyword");
-    len = all;
-    c = xmlGetUTF8Char(kwStart, &len);
-    if (!xmlStrchr(LATIN1_WS, c))
-      break;
-    kwStart += len;
-    all -= len;
-  }
+  char *kwStart = firstNonWS(line + 1);
+  if (!*kwStart)
+    fatalPSV("missing header keyword");
 
   // find keyword end, copy to buffer
-  char *kwEnd = kwStart + len;
-  all -= len;
+  char *kwEnd = kwStart;
+  size_t all = strlen(kwEnd);
+  int len;
   while (*kwEnd) {
-    c = xmlGetUTF8Char(kwEnd, &len);
+    len = all;
+    int c = xmlGetUTF8Char(kwEnd, &len);
     if (xmlStrchr(LATIN1_WS, c))
       break;
     kwEnd += len;
@@ -123,38 +185,31 @@ xmlNodePtr addHdr(xmlNodePtr parent)
     fatalPSV("long keyword");
   strncpy(kw, kwStart, kwLen);
   kw[kwLen] = 0;
-
-  // advance past whitespace to text following keyword
-  while (*kwEnd) {
-    c = xmlGetUTF8Char(kwEnd, &len);
-    if (!xmlStrchr(LATIN1_WS, c))
-      break;
-    kwEnd += len;
-    all -= len;
-  }
-
-  return xmlNewChild(parent, NULL, BAD_CAST kw, *kwEnd
-                     ? xmlEncodeEntitiesReentrant(doc, kwEnd)
+  char *txt = firstNonWS(kwEnd);
+  return xmlNewChild(parent, NULL, BAD_CAST kw, *txt
+                     ? xmlEncodeEntitiesReentrant(doc, txt)
                      : NULL);
 }
 
 // pxHeader
 //
-// on entry `line` contains a header line.
+// on entry `line` contains a top level (#) header line.
 // parse the observation header into the tree, skip blank lines,
 // on non-header, non-blank line, call pxObs.
 void pxHeader()
 {
   xmlNodePtr hdr = xmlNewChild(root_node, NULL,
                                BAD_CAST "observationContext", NULL);
-  xmlNodePtr h1 = addHdr(hdr);
+  xmlNodePtr h1 = addHdr(hdr);  // top level header
   while (getPSVLine()) {
     if (line[0] == '!')
       addHdr(h1);
     else if (line[0] == '#')
       h1 = addHdr(hdr);
-    else
+    else {
+      pxObs();
       break;
+    }
   }
 }
 
@@ -173,7 +228,7 @@ void px()
   do {
     if (*line == '#')
       pxHeader();
-    else if (*line != 0)
+    else
       pxObs();
   }
   while (getPSVLine());
