6e8f08fc3de5e16c6a41327a90688057fe29a8a4
Sonia Keys <skeys@cfa.harvard.edu>
2015-09-29 20:15:01 +0000

    add xs.c with readXMLFile, yet untested

---
+++ src/ades/xs.c
@@ -0,0 +1,27 @@
+
+#include <libxml/tree.h>
+
+#include <ds.h>
+
+int tv(xmlDocPtr doc, char *schema);
+void ts(xmlDocPtr doc, observationBatch ** obs);
+
+int readXMLFile(char *fn, observationBatch ** obs, char *schema)
+{
+  xmlDocPtr doc = xmlParseFile(fn);
+  if (!doc)
+    return -1; // xml functions emit error messages
+
+  // printf("read okay\n");
+
+  if (schema) {
+    int r = tv(doc, schema);
+    if (r) {
+      // xmlSaveFormatFileEnc("debug.xml", doc, "UTF-8", 1);
+      exit(r);                  // xml functions emit error messages
+    }
+    // printf("validate okay\n");
+  }
+
+  ts(doc, obs);
+}
