9095bc607929a051c78d9c10a992c76e3fc4d1df
Sonia Keys <skeys@cfa.harvard.edu>
2015-10-07 19:55:27 +0000

    parse header

--- src/ades/mt.c
+++ src/ades/mt.c
@@ -10,12 +10,13 @@
 #define _GNU_SOURCE
 #include <regex.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include <globals.h>
 
 FILE *fobs;
 regex_t rxHdr;
-_Bool rxCompiled;
+_Bool rxHdrCompiled;
 
 xmlNodePtr contactNode;
 xmlNodePtr observersNode;
@@ -23,6 +24,9 @@ xmlNodePtr measurersNode;
 _Bool contactNameSet;
 char *contactAddress;
 char *ackEmail;
+char *net;
+char *bnd;
+char *com;
 
 // note mtFileError functions also use globals from globals.c
 int mtFileError(char *msg)
@@ -74,30 +78,35 @@ int mtGetLine()
 
 _Bool mtMatchHdr()
 {
-  if (!rxCompiled) {
+  if (!rxHdrCompiled) {
     int r = regcomp(&rxHdr, "^[A-Z]{2}[A-Z2] ", REG_EXTENDED | REG_NOSUB);
     if (r) {
       regerror(r, &rxHdr, errLine, sizeof errLine);
       exit(-1);
     }
-    rxCompiled = 1;
+    rxHdrCompiled = 1;
   }
   return !regexec(&rxHdr, line, 0, NULL, 0);
 }
 
+xmlNodePtr newChild(xmlNodePtr parent, char *name, char *val)
+{
+  xmlNodePtr n = xmlNewChild(parent, NULL, name, val);
+  n->line = lineNum;
+  return n;
+}
+
 int mtCOD(char *val, xmlNodePtr ctx)
 {
-  xmlNodePtr obs = xmlNewChild(ctx, NULL, "observatory", NULL);
-  xmlNewChild(obs, NULL, "mpcCode", val);
+  xmlNodePtr obs = newChild(ctx, "observatory", NULL);
+  newChild(obs, "mpcCode", val);
   return 0;
 }
 
 int mtCON(char *val, xmlNodePtr ctx)
 {
-  if (!contactNode) {
-    contactNode = xmlNewChild(ctx, NULL, "contact", NULL);
-    contactNode->line = lineNum;
-  }
+  if (!contactNode)
+    contactNode = newChild(ctx, "contact", NULL);
 
   char *lb, *rb;
   if ((lb = strchr(val, '[')) && (rb = strchr(lb, ']'))) {
@@ -116,8 +125,7 @@ int mtCON(char *val, xmlNodePtr ctx)
     char *c = strchr(val, ',');
     if (c)
       *c = 0;
-    xmlNodePtr n = xmlNewChild(contactNode, NULL, "name", val);
-    n->line = lineNum;
+    newChild(contactNode, "name", val);
     contactNameSet = 1;
     if (!c)
       return 0;
@@ -148,7 +156,7 @@ int mtNameList(char *val, xmlNodePtr parent)
         continue;
       }
     }
-    xmlNewChild(parent, NULL, "name", val);
+    newChild(parent, "name", val);
     if (!c)
       break;
     val = c + 1;
@@ -159,26 +167,145 @@ int mtNameList(char *val, xmlNodePtr parent)
 int mtOBS(char *val, xmlNodePtr ctx)
 {
   if (!observersNode)
-    observersNode = xmlNewChild(ctx, NULL, "observers", NULL);
+    observersNode = newChild(ctx, "observers", NULL);
   return mtNameList(val, observersNode);
 }
 
 int mtMEA(char *val, xmlNodePtr ctx)
 {
   if (!measurersNode)
-    measurersNode = xmlNewChild(ctx, NULL, "measurers", NULL);
+    measurersNode = newChild(ctx, "measurers", NULL);
   return mtNameList(val, measurersNode);
+}
+
+void mtDesign(char *design, xmlNodePtr tel)
+{
+  FILE *f = fopen("telescopes.csv", "r");
+  if (!f)
+    goto des;
+  struct stat st;
+  if (fstat(fileno(f), &st))
+    goto des;
+  char *buf = malloc(st.st_size + 1);
+  fread(buf, st.st_size, 1, f);
+  buf[st.st_size] = 0;
+  fclose(f);
+  char *p;
+  int dl = strlen(design);
+  for (p = buf; (p = strstr(p, design)); p += dl) {
+    if (p != buf && p[-1] != '\n') // must start at beginning of line
+      continue;
+    if (p[dl] != ',')           // must end at comma
+      continue;
+    // design argument matches telescope name, actual design follows comma.
+    p += dl + 1;
+    char *q = strchr(p, '\n');
+    if (q)
+      *q = 0;
+    newChild(tel, "name", design);
+    newChild(tel, "design", p);
+    return;
+  }
+ des:
+  newChild(tel, "design", design);
 }
 
 int mtTEL(char *val, xmlNodePtr ctx)
 {
-  xmlNodePtr tel = xmlNewChild(ctx, NULL, "telescope", val);
+  xmlNodePtr tel = newChild(ctx, "telescope", NULL);
+  regex_t rxTel;
+  int r = regcomp(&rxTel, "^\
+([.0-9]+)-m \
+(f/([.0-9]+) )?\
+([0-9A-Za-z][0-9A-Za-z -]*[0-9A-Za-z])\
+( [+]( ([0-9]+K?)(x([0-9]+K?))?)? CCD)?\
+$", REG_EXTENDED);
+  if (r) {
+    regerror(r, &rxHdr, errLine, sizeof errLine);
+    exit(-1);
+  }
+  regmatch_t sub[10];
+  if (r = regexec(&rxTel, val, 10, sub, 0)) {
+    // no match, just add everything as design.
+    newChild(tel, "design", val);
+    return 0;
+  }
+  // aperture must match
+  val[sub[1].rm_eo] = 0;
+  newChild(tel, "aperture", val + sub[1].rm_so);
+
+  // focal ratio optional
+  int eo = sub[3].rm_eo;
+  if (eo > 0) {
+    val[eo] = 0;
+    newChild(tel, "fRatio", val + sub[3].rm_so);
+  }
+  // design/name must match
+  val[sub[4].rm_eo] = 0;
+  mtDesign(val + sub[4].rm_so, tel);
+
+  // size optional
+  if ((eo = sub[7].rm_eo) > 0) {
+    val[eo] = 0;
+    int e2 = sub[9].rm_eo;
+    if (e2 > 0) {
+      val[e2] = 0;
+    }
+    snprintf(line2, sizeof line2, "%s \u00d7 %s",
+             val + sub[7].rm_so, val + sub[e2 > 0 ? 9 : 7].rm_so);
+    newChild(tel, "arraySize", line2);
+  }
+  return 0;
+}
+
+int mtNET(char *val)
+{
+  if (net)
+    return mtFileError("multiple NET lines");
+  net = strdup(val);
+  return 0;
+}
+
+int mtBND(char *val)
+{
+  if (bnd)
+    return mtFileError("multiple BND lines");
+  bnd = strdup(val);
+  return 0;
+}
+
+int mtCOM(char *val) {
+  if (com)
+    asprintf(&com, "%s, %s", com, val);
+  else
+    com = strdup(val);
+  return 0;
+}
+
+int mtNUM(char *val, xmlNodePtr ctx)
+{
+  xmlNodePtr n = newChild(ctx, "observation", NULL);
+  newChild(n, "count", val);
+  return 0;
+}
+
+int mtACK(char *val, xmlNodePtr ctx) {
+  if (!contactNode)
+    contactNode = newChild(ctx, "contact", NULL);
+  newChild(contactNode, "ackMessage", val);
+  return 0;
+}
+
+int mtAC2(char *val) {
+  if (ackEmail)
+    asprintf(&ackEmail, "%s,%s", ackEmail, val);
+  else
+    ackEmail = strdup(val);
   return 0;
 }
 
 int mtHdrLine(xmlNodePtr ctx)
 {
-  printf("mtHdrLine: %s\n", line);
   if (!strncmp(line, "COD", 3))
     return mtCOD(line + 4, ctx);
   if (!strncmp(line, "CON", 3))
@@ -189,15 +316,24 @@ int mtHdrLine(xmlNodePtr ctx)
     return mtMEA(line + 4, ctx);
   if (!strncmp(line, "TEL", 3))
     return mtTEL(line + 4, ctx);
+  if (!strncmp(line, "NET", 3))
+    return mtNET(line + 4);
+  if (!strncmp(line, "BND", 3))
+    return mtBND(line + 4);
+  if (!strncmp(line, "COM", 3))
+    return mtCOM(line + 4);
+  if (!strncmp(line, "NUM", 3))
+    return mtNUM(line + 4, ctx);
+  if (!strncmp(line, "ACK", 3))
+    return mtACK(line + 4, ctx);
+  if (!strncmp(line, "AC2", 3))
+    return mtAC2(line + 4);
   return 0;
 }
 
 int mtHdrBlock()
 {
-  printf("mtHdrBlock\n");
-  xmlNodePtr ctx = xmlNewChild(root_node, NULL,
-                               "observationContext", NULL);
-  ctx->line = lineNum;
+  xmlNodePtr ctx = newChild(root_node, "observationContext", NULL);
   do {
     int r = mtHdrLine(ctx);
     if (r)
@@ -208,12 +344,18 @@ int mtHdrBlock()
   while (mtMatchHdr());
 
   if (contactAddress) {
-    xmlNewChild(contactNode, NULL, "address", contactAddress);
+    newChild(contactNode, "address", contactAddress);
     contactAddress = NULL;
   }
   if (ackEmail) {
-    xmlNewChild(contactNode, NULL, "ackEmail", ackEmail);
+    if (!contactNode)
+      contactNode = newChild(ctx, "contact", NULL);
+    newChild(contactNode, "ackEmail", ackEmail);
     ackEmail = NULL;
+  }
+  if (com) {
+    newChild(ctx, "comment", com);
+    com = NULL;
   }
   if (contactNode)
     contactNode = NULL;
@@ -226,13 +368,14 @@ int mtHdrBlock()
 
 int mtObsBlock()
 {
-  printf("mtObsBlock\n");
   do {
     int r = mtGetLine();
     if (r)
       return r;
   }
   while (*line);
+  net = NULL;
+  bnd = NULL;
   return 0;
 }
 
